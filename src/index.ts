a3a1775247c36eff27486012ab20e0f9c7e2d810
SamuelKasper <Samuel.Noah.Kasper@hs-furtwangen.de>
2023-02-17 11:17:41 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,117 @@
+window.onload = function () {
+    init();
+}
+
+/*let grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]];*/
+
+let grid = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 0, 0, 3, 6, 5, 8, 9, 7],
+    [3, 6, 0, 8, 9, 1, 2, 4, 5],
+    [5, 0, 0, 0, 4, 7, 3, 6, 1],
+    [6, 0, 0, 0, 0, 0, 0, 0, 0],
+    [8, 7, 0, 0, 3, 0, 0, 1, 0],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+function init() {
+    // Create number panel
+    for (let i = 1;i<=9;i++){
+        let number = document.createElement("div");
+        number.id = i.toString();
+        number.innerText = i.toString();
+        number.addEventListener("click", selectNr)
+        number.classList.add("numbers");
+        document.getElementById("numbers")?.append(number)
+    }
+
+    // Create board
+    let gameArea = document.getElementById("gameArea");
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + c.toString();
+            tile.innerText = grid[r][c].toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click",selectTile)
+            if (gameArea != null)
+                gameArea.append(tile);
+        }
+    }
+}
+
+// Select number from number panel
+let selection:HTMLDivElement;
+function selectNr(){
+    solve();
+    if(selection != null){
+        selection.classList.remove("selection");
+    }
+    selection = this;
+    selection.classList.add("selection");
+}
+
+// Put selected number into the board
+function selectTile(){
+    if(selection){
+        if(this.innerText != ""){
+            return;
+        }
+        this.innerText = selection.id;
+    }
+}
+
+function possible(y:number, x:number, n:number) {
+    for (let i = 0; i < 9; i++) {
+        if (grid[y][i] == n) {
+            return false;
+        }
+    }
+    for (let i = 0; i < 9; i++) {
+        if (grid[i][x] == n) {
+            return false;
+        }
+    }
+    let x0 = Math.floor(x / 3) * 3;
+    let y0 = Math.floor(y / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (grid[y0 + i][x0 + j] == n) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function solve() {
+    for (let y = 0; y < 9; y++) {
+        for (let x = 0; x < 9; x++) {
+            if (grid[y][x] == 0) {
+                for (let n = 1; n <= 9; n++) {
+                    if (possible(y, x, n)) {
+                        grid[y][x] = n;
+                        // Show nr on board 
+                        let tile = document.getElementById(y+""+x);
+                        tile!.innerText = n.toString();
+                        tile!.style.backgroundColor = "green";
+                        // End Show nr on board
+                        solve();
+                        grid[y][x] = 0;
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
